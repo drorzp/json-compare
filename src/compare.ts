2f2de5b1d8f607f1f51914f689b4ff5b41584a36
Dror Zeplovitch <drorzp@hotmail.com>
2022-02-04 15:43:22 -0500

    code review

--- src/compare.ts
+++ src/compare.ts
@@ -1,34 +1,42 @@
 import * as jsonpatch from 'fast-json-patch';
 import { format, getDifPosition, join } from './util';
-
+enum operation {
+    ADD = "add",
+    REMOVE = "remove",
+    REPLACE = "replace"
+}
+enum origin {
+    SOURCE = "source",
+    TARGET = "target",
+}
 export function compare(source: any, target: any) {
-    const diffList: { origin: string, action: string, start: number, end: number }[] = [];
+    const diffList: { origin: string, operation: string, start: number, end: number }[] = [];
     const diffs = jsonpatch.compare(source, target);
     let sourceStr: string = JSON.stringify(source),
         targetStr: string = JSON.stringify(target);
     diffs.forEach(function (diff) {
         try {
-            if (diff.op === 'remove') {
+            if (diff.op === operation.REMOVE) {
                 const pos = getDifPosition(sourceStr, diff);
-                diffList.push({ origin: 'source', action: 'remove', start: pos.start.ch, end: pos.end.ch })
-            } else if (diff.op === 'add') {
+                diffList.push({ origin: origin.SOURCE, operation: diff.op, start: pos.start.ch, end: pos.end.ch })
+            } else if (diff.op === operation.ADD) {
                 const pos = getDifPosition(targetStr, diff);
-                diffList.push({ origin: 'target', action: 'add', start: pos.start.ch, end: pos.end.ch })
-            } else if (diff.op === 'replace') {
+                diffList.push({ origin: origin.TARGET, operation: diff.op, start: pos.start.ch, end: pos.end.ch })
+            } else if (diff.op === operation.REPLACE) {
                 let pos = getDifPosition(sourceStr, diff);
-                diffList.push({ origin: 'source', action: "replace", start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: origin.SOURCE, operation: diff.op, start: pos.start.ch, end: pos.end.ch })
                 pos = getDifPosition(targetStr, diff);
-                diffList.push({ origin: 'target', action: 'replace', start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: origin.TARGET, operation: diff.op, start: pos.start.ch, end: pos.end.ch })
             }
         } catch (e) {
             console.warn('error while trying to highlight diff', e);
         }
     });
-    for (const item of diffList.filter(c => c.origin === "target").sort((a, b) => b.end - a.end)) {
-        targetStr = join(targetStr, item.start, item.end, item.action)
+    for (const item of diffList.filter(c => c.origin === origin.TARGET).sort((a, b) => b.end - a.end)) {
+        targetStr = join(targetStr, item.start, item.end, item.operation)
     }
-    for (const item of diffList.filter(c => c.origin === "source").sort((a, b) => b.end - a.end)) {
-        sourceStr = join(sourceStr, item.start, item.end, item.action)
+    for (const item of diffList.filter(c => c.origin === origin.SOURCE).sort((a, b) => b.end - a.end)) {
+        sourceStr = join(sourceStr, item.start, item.end, item.operation)
     }
     return [
         '<pre>' + format(sourceStr).replace(/spanclass/g, 'span class') + '</pre>',
