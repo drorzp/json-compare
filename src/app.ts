341f9f2cd5e51ac8834d42d96e2c422c119d9195
Dror Zeplovitch <drorzp@hotmail.com>
2022-02-04 14:04:48 -0500

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as jsonpatch from 'fast-json-patch';
 import { source, target } from './const';
-import { format } from './util';
+import { format, join } from './util';
 const jsonMap = require('json-source-map');
 
 
@@ -28,8 +28,7 @@ const getStartAndEndPosOfDiff = (textValue: string, diff: any) => {
 }
 
 const compare = (sourceStr: string, targetStr: string) => {
-    const targetArr: { start: string, end: string }[] = [];
-    const sourceArr: { start: string, end: string }[] = [];
+    const diffList: { origin: string, action: string, start: number, end: number }[] = [];
 
     const diffs = jsonpatch.compare(source, target);
 
@@ -37,34 +36,27 @@ const compare = (sourceStr: string, targetStr: string) => {
         try {
             if (diff.op === 'remove') {
                 const pos = getStartAndEndPosOfDiff(sourceStr, diff);
-                sourceArr.push({ start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: "source", action: "remove", start: pos.start.ch, end: pos.end.ch })
             } else if (diff.op === 'add') {
                 const pos = getStartAndEndPosOfDiff(targetStr, diff);
-                targetArr.push({ start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: "target", action: "add", start: pos.start.ch, end: pos.end.ch })
             } else if (diff.op === 'replace') {
                 let pos = getStartAndEndPosOfDiff(sourceStr, diff);
-                sourceArr.push({ start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: "source", action: "replace", start: pos.start.ch, end: pos.end.ch })
                 pos = getStartAndEndPosOfDiff(targetStr, diff);
-                targetArr.push({ start: pos.start.ch, end: pos.end.ch })
+                diffList.push({ origin: "target", action: "replace", start: pos.start.ch, end: pos.end.ch })
             }
         } catch (e) {
             console.warn('error while trying to highlight diff', e);
         }
     });
-    targetArr.sort((a, b) => +b.end - +a.end)
-    sourceArr.sort((a, b) => +b.end - +a.end)
-    for (const item of targetArr) {
-        const leftPart = targetStr.slice(0, +item.start);
-        const colorPart = targetStr.slice(+item.start, +item.end);
-        const rightPart = targetStr.slice(+item.end);
-        targetStr = leftPart + "<span class='additional'>" + colorPart + "</span>" + rightPart;
+
+    for (const item of diffList.filter(c => c.origin === "target").sort((a, b) => b.end - a.end)) {
+        targetStr = join(targetStr, item.start, item.end, item.action)
         targetDiv!.innerHTML = '<pre>' + format(targetStr).replace(/spanclass/g, 'span class') + '</pre>'
     }
-    for (const item of sourceArr) {
-        const leftPart = sourceStr.slice(0, +item.start);
-        const colorPart = sourceStr.slice(+item.start, +item.end);
-        const rightPart = sourceStr.slice(+item.end);
-        sourceStr = leftPart + "<span class='missing'>" + colorPart + "</span>" + rightPart;
+    for (const item of diffList.filter(c => c.origin === "source").sort((a, b) => b.end - a.end)) {
+        sourceStr = join(sourceStr, item.start, item.end, item.action)
         sourceDiv!.innerHTML = '<pre>' + format(sourceStr).replace(/spanclass/g, 'span class') + '</pre>'
     }
 }
--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const source = {
-    name: "Dror", color: "white"
+    name: "Dror", color: "white", city: "New York"
 };
 export const target = {
-    name: "Dror", age: 33, address: "10 spruce street"
+    name: "Dror", age: 33, address: "10 spruce street", city: "Tel Aviv"
 };
